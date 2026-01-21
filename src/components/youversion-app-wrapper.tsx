5caf9edc7f73c3a77b3e2de6abdbc803890fdf96
cameronapak <cameronandrewpak@gmail.com>
2026-01-21 16:17:32 -0600

    added page provider

--- src/components/youversion-app-wrapper.tsx
+++ src/components/youversion-app-wrapper.tsx
@@ -1,7 +1,7 @@
 import { YouVersionProvider } from '@youversion/platform-react-ui'
 import { type ReactElement } from 'react'
 
-export function App({ children, appKey }: { children: ReactElement; appKey: string }) {
+export function YouVersionAppWrapper({ children, appKey }: { children: ReactElement; appKey: string }) {
   return (
     <YouVersionProvider appKey={appKey} includeAuth={false}>
       {children}
