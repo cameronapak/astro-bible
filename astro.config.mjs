9369bf62bb78ffd485e30c5fe3d341fcef2cfe34
cameronapak <cameronandrewpak@gmail.com>
2026-01-21 16:42:29 -0600

    npx astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig, envField } from 'astro/config'
 import react from '@astrojs/react'
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   env: {
     schema: {
       YVP_PUBLIC_APP_KEY: envField.string({ context: 'client', access: 'public', optional: false }),
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
