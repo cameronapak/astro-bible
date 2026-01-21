a781308b72bd74b8e259e8186105440e89023a0a
cameronapak <cameronandrewpak@gmail.com>
2026-01-21 16:13:28 -0600

    added env variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  integrations: [react()],
+  env: {
+    schema: {
+      YVP_PUBLIC_APP_KEY: envField.string({ context: 'client', access: 'public', optional: false }),
+    },
+  },
+})
